3aef5f166cdfa11b9f6575ecd66e1409c69bd17e
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-04-07 21:26:55 +0100

    Deploy frontend

--- sample-frontend/docker/dockerize.sh
+++ sample-frontend/docker/dockerize.sh
@@ -3,7 +3,7 @@
 APP_VERSION=$(awk '/version/' package.json | cut -d ':' -f2 | cut -d '"' -f2)
 
 # build frontend image
-docker build -t brunofjesus/sample-frontend:$APP_VERSION -f ./docker/Dockerfile .
+docker buildx build -t brunofjesus/sample-frontend:$APP_VERSION -f ./docker/Dockerfile --push --platform=linux/arm64,linux/amd64 .
 
 # push
-docker push brunofjesus/sample-frontend:$APP_VERSION
+#docker push brunofjesus/sample-frontend:$APP_VERSION
